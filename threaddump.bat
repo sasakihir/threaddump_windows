219bc2caae4342318c63aaf329b4a46a09b93b78
sasakihir <sasaki_hir@works-hi.co.jp>
2020-09-01 10:32:16 +0900

    change readme to markdown

--- threaddump.bat
+++ threaddump.bat
@@ -15,14 +15,15 @@ REM processing
 REM ####################
 
 set log_dir=%~dp0\log
+mkdir %log_dir% 2>null
 set path=%jdk_home%\bin;%pstools_dir%;%path%
 
 for /l %%n in (1,1,%times%) do (
-  set time_tmp=%time: =0%
-	set now=%date:/=%%time_tmp:~0,2%%time_tmp:~3,2%%time_tmp:~6,2%
-	jstack.exe %pid% > %log_dir%\threaddump.%now%.log 2> %log_dir%\threaddump.%now%.err.log
-	REM psexec -accepteula cmd /c "jstack.exe %pid% > %log_dir%\threaddump.%now%.log 2> %log_dir%\threaddump.%now%.err.log"
-  timeout %waits%
+  set time_tmp=!time: =0!
+	set now=!date:/=!!time_tmp:~0,2!!time_tmp:~3,2!!time_tmp:~6,2!
+	jstack.exe !pid! > !log_dir!\threaddump.!now!.log 2> !log_dir!\threaddump.!now!.err.log
+	REM psexec -accepteula cmd /c "jstack.exe !pid! > !log_dir!\threaddump.!now!.log 2> !log_dir!\threaddump.!now!.err.log"
+  timeout !waits!
 )
 
 exit
